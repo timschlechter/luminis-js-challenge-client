11edb77349964161b9e01a6c3939fc3cd67e8bb9
Tim Schlechter <timschlechter@gmail.com>
2013-03-30 10:50:34 +0100

    Deploy test

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,5 @@
-node less2css.js
-uglifycss ../app/css/styles.css > ../app/css/styles.min.css
+# Compile LESS files to CSS
+../node_modules/less/bin/lessc ../app/styles/styles.less > ../app/css/styles.css
+
+# Minify CSS
+../node_modules/uglifycss/uglifycss ../app/css/styles.css > ../app/css/styles.min.css
