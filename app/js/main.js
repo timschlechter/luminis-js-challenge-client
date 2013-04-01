203dba3756af69d60a5bea6944d0e55b5d68ea80
Tim Schlechter <timschlechter@gmail.com>
2013-04-01 09:45:10 +0200

    Reorganised files/folders

--- app/js/main.js
+++ app/js/main.js
@@ -23,3 +23,24 @@ chatApp.config(
 chatApp.config(['$httpProvider', function($httpProvider) {
 	delete $httpProvider.defaults.headers.common["X-Requested-With"];
 }]);
+
+// Add a filter for truncating text
+angular.module('filters', []).
+    filter('truncate', function () {
+        return function (text, length) {
+            if (!text)
+                return;
+
+            if (isNaN(length))
+                length = 10;
+
+            var end = "...";
+
+            if (text.length <= length || text.length - end.length <= length) {
+                return text;
+            }
+            else {
+                return String(text).substring(0, length-end.length) + end;
+            }
+        };
+});
