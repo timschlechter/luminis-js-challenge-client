4923354e3c54e2f0db9d66a036f0729bc4f9f4b8
Tim Schlechter <timschlechter@gmail.com>
2013-04-05 23:06:17 +0200

    Modified build

--- deploy.sh
+++ deploy.sh
@@ -106,13 +106,14 @@ cd build
 ./build.sh
 cd ..
 
+# Switch develop/production index.html
+rm app/index.html
+mv app/production.html app/index.html
+
 # 4. KuduSync
 $KUDU_SYNC_CMD -v 50 -f "$DEPLOYMENT_SOURCE/app" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh" 2> /dev/null
 exitWithMessageOnError "Kudu Sync failed"
 
-# 5. Switch develop/production index.html
-rm app/index.html
-mv app/production.html app/index.html
 
 ##################################################################################################################################
 
